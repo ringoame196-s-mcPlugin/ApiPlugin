79f234f56eed113e2ddbbd634e11b27d56793fbb
りんご飴196 <132573268+Ringoame196@users.noreply.github.com>
2025-09-21 14:11:13 +0900

    🗑️ 不必要なイベントクラス コマンドクラスの削除

--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/Main.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/Main.kt
@@ -1,6 +1,5 @@
 package com.github.ringoame196_s_mcPlugin
 
-import com.github.ringoame196_s_mcPlugin.events.Events
 import org.bukkit.plugin.java.JavaPlugin
 
 class Main : JavaPlugin() {
@@ -8,9 +7,6 @@ class Main : JavaPlugin() {
     override fun onEnable() {
         super.onEnable()
         ApiServer.startup()
-        server.pluginManager.registerEvents(Events(), plugin)
-        // val command = getCommand("command")
-        // command!!.setExecutor(Command())
     }
 
     override fun onDisable() {
