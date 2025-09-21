99c8b8107943ed67f9a2dc35148ae41d022755b4
りんご飴196 <132573268+Ringoame196@users.noreply.github.com>
2025-09-21 14:12:15 +0900

    ✨ 単一データ 複数データ 両方対応

--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/JsonResponder.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/JsonResponder.kt
@@ -5,6 +5,15 @@ import com.sun.net.httpserver.HttpExchange
 object JsonResponder {
     fun respondJson(exchange: HttpExchange, statusCode: Int, data: Map<String, Any?>) {
         val json = JsonManager.jsonString(data)
+        respondJson(exchange, statusCode, json)
+    }
+
+    fun respondJson(exchange: HttpExchange, statusCode: Int, data: List<Map<String, Any?>>) {
+        val json = JsonManager.jsonString(data)
+        respondJson(exchange, statusCode, json)
+    }
+
+    private fun respondJson(exchange: HttpExchange, statusCode: Int, json: String) {
         exchange.responseHeaders.add("Content-Type", "application/json; charset=utf-8")
         val bytes = json.toByteArray(Charsets.UTF_8)
         exchange.sendResponseHeaders(statusCode, bytes.size.toLong())
