d83b7c1c597cf3ca439eda3edebfb79be90940d5
りんご飴196 <132573268+Ringoame196@users.noreply.github.com>
2025-09-21 14:19:22 +0900

    🗑️ plugin削除

--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/Main.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/Main.kt
@@ -3,7 +3,6 @@ package com.github.ringoame196_s_mcPlugin
 import org.bukkit.plugin.java.JavaPlugin
 
 class Main : JavaPlugin() {
-    private val plugin = this
     override fun onEnable() {
         super.onEnable()
         ApiServer.startup()
