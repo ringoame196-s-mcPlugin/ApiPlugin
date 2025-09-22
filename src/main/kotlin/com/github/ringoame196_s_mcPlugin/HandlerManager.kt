f6808178031fb65307d7a67706d1e12036e08762
りんご飴196 <132573268+Ringoame196@users.noreply.github.com>
2025-09-22 15:01:26 +0900

    ✨ name取得を共通化

---
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/HandlerManager.kt
@@ -0,0 +1,12 @@
+package com.github.ringoame196_s_mcPlugin
+
+import com.sun.net.httpserver.HttpExchange
+
+object HandlerManager {
+    fun getPathParam(exchange: HttpExchange, index: Int): String? {
+        val parts = exchange.requestURI.path.split("/")
+        return if (parts.size > index && parts[index].isNotEmpty()) {
+            parts[index]
+        } else null
+    }
+}
